3e0ea490667f3f409f41875e435480f7a01c6b64
George <georgezmsje@yahoo.com>
2017-11-26 22:36:54 -0800

    Ok so it was a little harder than I expected since I had some vodka just now but now the update button will start off as being disabled and will only enable itself when it detects some kind of change. Also I made it so that clicking on the text for the checkboxes (send messages/show notifications) will also toggle the checkbox in case the person using it is also drunk and having trouble clicking on the tiny checkbox (please dont drink and drive).

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,11 +1,38 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class Config {
 
     private JFrame jfrm;
 
+    String f;
+    String c;
+    String r;
+    boolean n;
+    boolean m;
+    JLabel feedLabel = new JLabel("Feed");
+    JLabel citiesLabel = new JLabel("Cities");
+    JLabel receiverLabel = new JLabel("Receiver");
+    JLabel showNotificationsLabel = new JLabel("Show Notifications");
+    JLabel sendMessagesLabel = new JLabel("Send Text Messages");
+    JTextField feedField = new JTextField(20);
+    JTextField receiverField = new JTextField(20);
+    JTextArea citiesTextArea = new JTextArea();
+    JCheckBox showNotificationsCheckbox = new JCheckBox();
+    JCheckBox sendMessagesCheckbox = new JCheckBox();
+    JButton update = new JButton("Update");
+
     public Config(String feed, String cities, String receiver, boolean showNotifications, boolean sendMessages) {
+        f = feed;
+        c = cities;
+        r = receiver;
+        n = showNotifications;
+        m = sendMessages;
+
         jfrm = new JFrame("Configuration");
 
         JPanel panel1 = new JPanel(new GridLayout(2, 1, 1, 1));
@@ -16,30 +43,114 @@ public class Config {
         JPanel panel6 = new JPanel(new FlowLayout());
         JPanel panel7 = new JPanel(new FlowLayout());
 
-        JLabel feedLabel = new JLabel("Feed");
-        JLabel citiesLabel = new JLabel("Cities");
-        JLabel receiverLabel = new JLabel("Receiver");
-        JLabel showNotificationsLabel = new JLabel("Show Notifications");
-        JLabel sendMessagesLabel = new JLabel("Send Text Messages");
-        JTextField feedField = new JTextField(20);
-        JTextField receiverField = new JTextField(20);
-        JTextArea citiesTextArea = new JTextArea();
-        JCheckBox showNotificationsCheckbox = new JCheckBox();
-        JCheckBox sendMessagesCheckbox = new JCheckBox();
-        JButton update = new JButton("Update");
-
         feedField.setText(feed);
         citiesTextArea.setText(cities);
         receiverField.setText(receiver);
         showNotificationsCheckbox.setSelected(showNotifications);
         sendMessagesCheckbox.setSelected(sendMessages);
         receiverField.setEnabled(sendMessages);
+        update.setEnabled(false);
 
-        update.addActionListener((actionEvent) -> {
-            RareNotifier.updateSettings(feedField.getText(), citiesTextArea.getText(), receiverField.getText(), showNotificationsCheckbox.isSelected(), sendMessagesCheckbox.isSelected());
+        KeyListener defaultKeyListener = new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (areSettingsDifferent()) {
+                    update.setEnabled(true);
+                } else {
+                    update.setEnabled(false);
+                }
+            }
+        };
+
+        feedField.addKeyListener(defaultKeyListener);
+        citiesTextArea.addKeyListener(defaultKeyListener);
+        receiverField.addKeyListener(defaultKeyListener);
+        showNotificationsCheckbox.addActionListener((actionEvent) -> {
+            if (areSettingsDifferent()) {
+                update.setEnabled(true);
+            } else {
+                update.setEnabled(false);
+            }
+        });
+        showNotificationsLabel.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                showNotificationsCheckbox.setSelected(!showNotificationsCheckbox.isSelected());
+                if (areSettingsDifferent()) {
+                    update.setEnabled(true);
+                } else {
+                    update.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+            }
         });
         sendMessagesCheckbox.addActionListener((actionEvent) -> {
             receiverField.setEnabled(sendMessagesCheckbox.isSelected());
+            if (areSettingsDifferent()) {
+                update.setEnabled(true);
+            } else {
+                update.setEnabled(false);
+            }
+        });
+        sendMessagesLabel.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                sendMessagesCheckbox.setSelected(!sendMessagesCheckbox.isSelected());
+                receiverField.setEnabled(sendMessagesCheckbox.isSelected());
+                if (areSettingsDifferent()) {
+                    update.setEnabled(true);
+                } else {
+                    update.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+            }
+        });
+        update.addActionListener((actionEvent) -> {
+            f = feedField.getText();
+            c = citiesTextArea.getText();
+            r = receiverField.getText();
+            n = showNotificationsCheckbox.isSelected();
+            m = sendMessagesCheckbox.isSelected();
+            RareNotifier.updateSettings(f, c, r, n, m);
+            update.setEnabled(false);
         });
 
         panel7.add(feedLabel);
@@ -67,10 +178,29 @@ public class Config {
         jfrm.setVisible(true);
     }
 
-    static void showConfig(String feed, String cities, String receiver, boolean showNotifications, boolean sendMessages) {
+    public static void showConfig(String feed, String cities, String receiver, boolean showNotifications, boolean sendMessages) {
         SwingUtilities.invokeLater(() -> {
             new Config(feed, cities, receiver, showNotifications, sendMessages);
         });
+    }
+
+    private boolean areSettingsDifferent() {
+        if (!f.equals(feedField.getText())) {
+            return true;
+        }
+        if (!c.equals(citiesTextArea.getText())) {
+            return true;
+        }
+        if (!r.equals(receiverField.getText())) {
+            return true;
+        }
+        if (n != showNotificationsCheckbox.isSelected()) {
+            return true;
+        }
+        if (m != sendMessagesCheckbox.isSelected()) {
+            return true;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
